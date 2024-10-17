48de7016c906aa938bde9ba6651b19f6e64d9941
jmbol <jmbol@169.254.46.68>
2024-10-17 12:26:50 -0500

    studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -15,8 +15,7 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
-		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2, 2));
 		return distance;
 	}
 
@@ -35,16 +34,20 @@ public class Methods {
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x,y, (3*radius/4));
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x,y, (radius/2));
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x,y, (radius/4));
 		
 	}
 
@@ -74,8 +77,9 @@ public class Methods {
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
-		// FIXME: Compute the sum of the values in an array
-		
+		for (int i=0;i<values.length;i++) {
+			sum = sum + values[i];
+		}
 		return sum;
 	}
 
@@ -87,8 +91,12 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
-		// TODO: Finish this method
+		int[] values = new int [length];// FIXME: Create an array of the appropriate size
+		
+		for (int i=0; i< values.length; i++) {
+			values[i] = value; 
+		}
+		
 
 		
 
@@ -97,6 +105,14 @@ public class Methods {
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
-
+/**
+ * 
+ * @param values array filled with values
+ * @return mean of the given array's values
+ */
+	public static double arrayMean(int[] values) {
+		double mean = arraySum(values)/values.length;
+		return mean;
+	}
 	
 }
